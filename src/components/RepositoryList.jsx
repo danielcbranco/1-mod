92572c847d1f54fcb412f15e8ae8d23803ee62e7
danielcbranco <daniel-cb1@hotmail.com>
2021-05-21 12:20:55 -0300

    sexto commit

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,29 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss'
+import { useState } from "react";
+
+// https://api.github.com/orgs/rocketseat/repos
+
 const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
+	name: 'unform',
+	description: 'Forms in React',
+	link: 'https://github.com/unform/unform'
 }
 
 export function RepositoryList(){
-    return(
-        <section className="repository-list"> 
-            <h1>Lista de repositórios</h1>
+	const [repositories, setRepositories] = useState([]);
 
-            <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-            </ul>
-        </section>
-    );
+	return(
+		<section className="repository-list"> 
+			<h1>Lista de repositórios</h1>
+
+			<ul>
+					<RepositoryItem repository={repository} />
+					<RepositoryItem repository={repository} />
+					<RepositoryItem repository={repository} />
+					<RepositoryItem repository={repository} />
+			</ul>
+		</section>
+	);
 };
